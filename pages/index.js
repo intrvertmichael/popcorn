2321dc6cbc20444767598f119277ac0e3b15db84
intrvertmichael <michaelpaguay@gmail.com>
2021-07-15 19:37:47 -0400

    fetching genres list & movies from specific genre

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,25 @@
-
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Trending from '../components/trending'
 
 export async function getStaticProps () {
-  const baseURL = "https://api.themoviedb.org/3/"
-  const key = process.env.MOVIE_KEY
 
-  const res = await fetch(baseURL +"/trending/movie/week?api_key=" + key)
-  const data = await res.json()
+  const movies = await getTrending()
+  const genres = await getGenres()
+  const actionMovies= await getActionMovies()
 
-  const movies = []
-
-  for(let i = 0; i < 16; i++){
-    let movie = data.results[i]
-    const imagePath = `https://image.tmdb.org/t/p/original/${movie.poster_path}`
-    movies.push ({
-      id: movie.id,
-      title: movie.original_title,
-      image: imagePath
-    })
-  }
+  console.log("genres", genres)
 
   return {
     props: {
-      movies: movies
+      movies,
+      genres,
+      actionMovies
     }
   }
 }
 
-export default function Home({movies}) {
+export default function Home({movies, genres, actionMovies}) {
   return (
     <>
       <Trending movies={movies} />
@@ -51,9 +42,75 @@ export default function Home({movies}) {
             <li> laugh </li>
             <li> cry </li>
           </ul>
+
         </div>
+
+        <ul>
+        {genres.map( (genre, key) => <li key={key}>{genre.name}</li>)}
+        </ul>
+
+        <h3>Animated Movies</h3>
+        <ol className={styles.genre_movies}>
+          {actionMovies? actionMovies.map( movie => {
+            const img = 'https://image.tmdb.org/t/p/original/' + movie.poster_path
+            return (
+              <li key={movie.id}>
+                <Image src={img} alt={movie.original_title} width="192" height="288"/>
+              </li>
+            )
+          }):''}
+        </ol>
 
       </div>
     </>
   )
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+async function getTrending() {
+  const baseURL = "https://api.themoviedb.org/3/"
+  const key = process.env.MOVIE_KEY
+
+  const res = await fetch(baseURL +"/trending/movie/week?api_key=" + key)
+  const data = await res.json()
+
+  const movies = []
+
+  for(let i = 0; i < 16; i++){
+    let movie = data.results[i]
+    const imagePath = `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+    movies.push ({
+      id: movie.id,
+      title: movie.original_title,
+      image: imagePath
+    })
+  }
+
+  return movies
+
+}
+
+async function getGenres() {
+  const baseURL = "https://api.themoviedb.org/3/"
+  const key = process.env.MOVIE_KEY
+
+  const res = await fetch(baseURL +"/genre/movie/list?api_key=" + key)
+  const data = await res.json()
+
+  return data.genres
+}
+
+async function getActionMovies() {
+
+  const baseURL = "https://api.themoviedb.org/3"
+  const key = process.env.MOVIE_KEY
+
+  const res = await fetch(baseURL +"/discover/movie?api_key=" + key + "&with_genres=16" )
+  // get genre movies = &with_genres=16
+  // get more pages = &page=2
+  const data = await res.json()
+
+  return data.results
 }
