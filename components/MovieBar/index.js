ded5e4bc70b8fec2d5d5a9458a11841dbf41eb13
intrvertmichael <michaelpaguay@gmail.com>
2021-07-28 20:00:21 -0400

    width programmatically set depending on length of movies

--- components/MovieBar/index.js
+++ components/MovieBar/index.js
@@ -6,7 +6,7 @@ const MovieBar = ({movieList}) => {
         <div className={styles.movie_bar}>
             <h3 className={styles.genre_movies_title}> {movieList.title} </h3>
             <div className={styles.genre_movies_wrapper}>
-                <ul className={styles.genre_movies}>
+                <ul className={styles.genre_movies} style={{width: `${movieList.movies.length * 16.7}%`}}>
                 { movieList.movies.map( movie => <Movie movie={movie} key={movie.id} /> ) }
                 </ul>
             </div>
