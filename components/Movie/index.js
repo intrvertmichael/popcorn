4fa794c0171dc1e599e8d1155d51f95ccdcfd01b
intrvertmichael <michaelpaguay@gmail.com>
2021-08-05 19:39:04 -0400

    added like and dislike buttons to movies

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -2,37 +2,60 @@ import Link from 'next/link'
 import Image from 'next/image'
 import {createMovieImageURL} from '../../requests/movie.api'
 import styles from '../../styles/Movie.module.css'
+import { useGetFirebaseUser } from '../../context/FirebaseContext'
 
 const Movie = ({movie}) => {
+    const firebaseUser = useGetFirebaseUser()
 
+    // title
     const titleLimit = 20
     const title = movie.original_title.length > titleLimit ?
-                        movie.original_title.substring(0, titleLimit) + '...'
-                        : movie.original_title
+                    movie.original_title.substring(0, titleLimit) + '...'
+                    : movie.original_title
     const poster = createMovieImageURL(movie.poster_path)
 
-    if(!movie.poster_path) return false
+    // event handlers
+    function liked_movie() {
+        console.log(firebaseUser.name, "likes", movie.original_title)
+    }
 
+    function disliked_movie() {
+        console.log(firebaseUser.name, "DOES NOT like", movie.original_title)
+    }
+
+    if(!movie.poster_path) return false
     return (
         <li className={styles.movie_li} key={movie.id}>
 
             <Image src={poster} alt={movie.original_title} width="288" height="432"/>
 
-            <div className={styles.movie_info}>
-                <div>
-                    <h3>
-                        <Link href={'/movie/' + movie.id}>
-                            <a>
-                                {title}
-                            </a>
-                        </Link>
-                    </h3>
+            <div
+                className={styles.movie_info}
+                style={firebaseUser? {} : {justifyContent:"center"}}
+            >
+                    <div className={styles.movie_info_description}>
+                        <div>
+                            <h3>
+                                <Link href={'/movie/' + movie.id}>
+                                    <a>
+                                        {title}
+                                    </a>
+                                </Link>
+                            </h3>
 
-                    <p>
-                        Voted {movie.vote_average} <br/>
-                        by {movie.vote_count} people
-                    </p>
-                </div>
+                            <p>
+                                ★ {movie.vote_average}
+                            </p>
+                        </div>
+                    </div>
+
+                    {
+                        firebaseUser &&
+                        <div className={styles.votes}>
+                            <button onClick={liked_movie}>👍</button>
+                            <button onClick={disliked_movie}>👎</button>
+                        </div>
+                    }
             </div>
         </li>
     )
