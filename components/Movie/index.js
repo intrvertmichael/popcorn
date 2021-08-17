2a5cc8ecb770bc82f564af0d1499e94bbfc6e26d
intrvertmichael <michaelpaguay@gmail.com>
2021-08-17 16:45:18 -0400

    movie bar will not show disliked movies

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -1,12 +1,17 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import {createMovieImageURL} from '../../requests/movie.api'
 import styles from '../../styles/Movie.module.css'
 import { useGetFirebaseUser } from '../../context/FirebaseContext'
 
-const Movie = ({movie}) => {
-
+const Movie = ({movie, fb_liked}) => {
     const firebaseUser = useGetFirebaseUser()
+    const [liked, setLiked] = useState()
+
+    useEffect(()=>{
+        if(fb_liked) setLiked(true)
+    }, [fb_liked])
 
     // title
     const titleLimit = 20
@@ -28,6 +33,9 @@ const Movie = ({movie}) => {
                 movie_id: movie.id,
             }
         })
+
+        if(liked) setLiked(null)
+        else setLiked(true)
     }
 
     async function disliked_movie() {
@@ -39,13 +47,22 @@ const Movie = ({movie}) => {
                 movie_id: movie.id,
             }
         })
+
+        if(liked===false) setLiked(null)
+        else setLiked(false)
+        // ToDo: figure out how to update the context everytime the button is pressed
     }
+
+
+    let classes = styles.movie_li
+    if(liked) classes = styles.liked
+    if(liked === false) classes = styles.disliked
 
     if(!movie.poster_path) return false
     return (
-        <li className={styles.movie_li} key={movie.id}>
+        <li className={classes} key={movie.id}>
 
-            <Image src={poster} alt={movie.original_title} width="288" height="432"/>
+            <Image src={poster} alt={movie.original_title} width="288" height="432" />
 
             <div
                 className={styles.movie_info}
--- components/MovieBar/index.js
+++ components/MovieBar/index.js
@@ -2,13 +2,31 @@ import Link from 'next/link'
 import styles from '../../styles/MovieBar.module.css'
 import Movie from '../Movie'
 
+import {useGetFirebaseUser} from '../../context/FirebaseContext'
+
 const MovieBar = ({movieList}) => {
+    const firebaseUser = useGetFirebaseUser()
 
     if(!movieList || movieList.movies.length <= 1) return false
-
-    const movie_length = movieList.movies.length
     const movie_image_size = 20
-    const ul_width = movie_length * movie_image_size
+
+    const movies = []
+    movieList.movies.map( movie => {
+        // check if movie is disliked
+        const fb_disliked = firebaseUser && firebaseUser.disliked?
+        firebaseUser.disliked.find(m => m.movie_id.toString() === movie.id.toString()) : null
+
+        // check if movie is liked
+        const fb_liked = firebaseUser && firebaseUser.liked?
+        firebaseUser.liked.find(m => m.movie_id.toString() === movie.id.toString()) : null
+
+        const liked = fb_liked? true : null
+        // if movie is not disliked then show it on the bar
+        if(!fb_disliked) movies.push(<Movie movie={movie} key={movie.id} fb_liked={liked}/>)
+    })
+
+    if(movies.length === 0) return false
+    const ul_width = movies.length * movie_image_size
 
     return (
         <div className={styles.movie_bar}>
@@ -20,7 +38,7 @@ const MovieBar = ({movieList}) => {
 
             <div className={styles.genre_movies_wrapper}>
                 <ul className={styles.genre_movies} style={{width: `${ul_width}%`}}>
-                    { movieList.movies.map( movie => <Movie movie={movie} key={movie.id} /> ) }
+                    {movies}
                 </ul>
             </div>
         </div>
