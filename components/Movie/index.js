f9a2850c4e384a471fbca6ef9091afc9331cae31
intrvertmichael <michaelpaguay@gmail.com>
2021-09-24 20:26:11 -0400

    added fe button to update dislikes on firebase

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -13,18 +13,28 @@ const Movie = ({movie, fb_liked}) => {
     const firebaseUser = useGetFirebaseUser()
     const [liked, setLiked] = useState()
 
+
+    // SETTING DEFAULT MOVIE VIEW
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     useEffect( () => {
         if(fb_liked) setLiked(true)
         else setLiked(null)
     }, [fb_liked])
 
-    // title
     const titleLimit = 20
     const title = movie.original_title.length > titleLimit ?
                     movie.original_title.substring(0, titleLimit) + '...'
                     : movie.original_title
     const poster = createMovieImageURL(movie.poster_path)
 
+    let classes = styles.movie_li
+    if(liked) classes = styles.liked
+    if(liked === false) classes = styles.disliked
+    // null has no style
+
+
+    // GETTING CURRENT FIREBASE MOVIES
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     async function getCurrentFirebaseMovies(){
         const fb_movie_res = await db.collection("movies").doc(firebaseUser.uid).get()
         const fb_movie_data = fb_movie_res.data()
@@ -37,30 +47,32 @@ const Movie = ({movie, fb_liked}) => {
         return {current_likes, current_dislikes}
     }
 
+    // LIKED BUTTON LISTENER
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     async function liked_movie() {
         const {current_likes, current_dislikes} = await getCurrentFirebaseMovies()
 
         if(liked) {
             setLiked(null)
-            // remove from liked list
+
             const updated_likes = current_likes.filter( m => m.movie_id !== movie.id )
             await db.collection("movies").doc(firebaseUser.uid).update({
                 liked: updated_likes
             })
+
             console.log("removed from likes...")
         }
 
         else {
             setLiked(true)
 
-            // add to like list on firebase
             await db.collection("movies").doc(firebaseUser.uid).update({
                 liked: [ ...current_likes, {movie_id: movie.id} ]
             })
 
             console.log("added to likes...")
 
-            // remove from disliked list
+
             const updated_dislikes = current_dislikes.filter( m => m.movie_id !== movie.id )
             await db.collection("movies").doc(firebaseUser.uid).update({
                 disliked: updated_dislikes
@@ -72,25 +84,49 @@ const Movie = ({movie, fb_liked}) => {
         }
     }
 
+    // DISLIKED BUTTON LISTENER
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     async function disliked_movie() {
-        // await fetch('/api/firebase/movies', {
-        //     method: 'POST',
-        //     headers: {
-        //         user_id: firebaseUser.uid,
-        //         movie_id: movie.id,
-        //     }
-        // })
-        if(liked===false) setLiked(null)
-        else setLiked(false)
+        const {current_likes, current_dislikes} = await getCurrentFirebaseMovies()
+
+        if(liked === false) {
+            setLiked(null)
+
+            const updated_dislikes = current_dislikes.filter( m => m.movie_id !== movie.id )
+            await db.collection("movies").doc(firebaseUser.uid).update({
+                disliked: updated_dislikes
+            })
+
+            console.log("removed from dislikes...")
+        }
+
+        else {
+            setLiked(false)
+
+            await db.collection("movies").doc(firebaseUser.uid).update({
+                disliked: [ ...current_dislikes, {movie_id: movie.id} ]
+            })
+
+            console.log("added to dislikes...")
+
+
+            const updated_likes = current_likes.filter( m => m.movie_id !== movie.id )
+            await db.collection("movies").doc(firebaseUser.uid).update({
+                liked: updated_likes
+            })
+
+            console.log("removed from likes...")
+
+
+            // TODO: add genre counter
+        }
     }
 
 
-    let classes = styles.movie_li
-    if(liked) classes = styles.liked
-    if(liked === false) classes = styles.disliked
-    // null has no style
+    // RENDERING MOVIE
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    if(!movie.poster_path) return false
+    if(!movie && !movie.poster_path) return false
 
     return (
         <li className={classes} key={movie.id}>
