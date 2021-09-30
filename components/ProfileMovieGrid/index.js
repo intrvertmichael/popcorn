1c59271614e111b57065b6529d14d21c7d0dd351
intrvertmichael <michaelpaguay@gmail.com>
2021-09-30 18:31:58 -0400

    remove from list updated style added confirm

--- components/ProfileMovieGrid/index.js
+++ components/ProfileMovieGrid/index.js
@@ -11,30 +11,44 @@ const ProfileMovieGrid = ({movies, set, likes}) => {
 
     if(movies.length === 0) return false
 
+
     return (
         <ul className={styles.genre_movies}>
             {
                 movies.map( movie => {
                     const poster = createMovieImageURL(movie.poster_path)
+                    const list = likes? "Likes" : "Dislikes"
+                    const message = `Are you sure you want to remove ${movie.original_title} from ${list} ?`
+
                     return (
                         <li key={movie.id}>
                             <Image src={poster} alt={movie.original_title} width="144" height="216" />
                             <div className={styles.movie_caption}>
-                                <h4>{movie.original_title}</h4>
                                 {
                                     likes?
-                                    <button onClick={() =>{
-                                        removeLiked(movie, firebaseUser)
-                                        const filtered = movies.filter( m => m.id !== movie.id)
-                                        set(filtered)
+                                    <button
+                                        className={styles.remove}
+                                        onClick={() =>{
+                                            const result = window.confirm(message);
+                                            if(result){
+                                                removeLiked(movie, firebaseUser)
+                                                const filtered = movies.filter( m => m.id !== movie.id)
+                                                set(filtered)
+                                            }
                                     }}> Remove from Likes </button>
                                     :
-                                    <button onClick={() => {
-                                        removeDisliked(movie, firebaseUser)
-                                        const filtered = movies.filter( m => m.id !== movie.id)
-                                        set(filtered)
+                                    <button
+                                        className={styles.remove}
+                                        onClick={() => {
+                                            const result = window.confirm(message);
+                                            if(result){
+                                                removeDisliked(movie, firebaseUser)
+                                                const filtered = movies.filter( m => m.id !== movie.id)
+                                                set(filtered)
+                                            }
                                     }}> Remove from Dislikes </button>
                                 }
+                                <h4>{movie.original_title}</h4>
                             </div>
                         </li>
                     )
