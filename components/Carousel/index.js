c6208ffdd19334e73c5b67e7bd7ab528711a8f98
intrvertmichael <michaelpaguay@gmail.com>
2021-10-12 18:56:28 -0400

    fixed bug remembering past image pos

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -1,22 +1,28 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from '../../styles/Carousel.module.css'
 
 const Carousel = ({images}) => {
-    const randomBg = images? Math.floor(images.length * Math.random()): 0
-    const [pos, setPos] = useState(randomBg)
+
+    const [pos, setPos] = useState(0)
+
+    useEffect(() => {
+        const randomBg = images? Math.floor(images.length * Math.random()): 0
+        setPos(randomBg)
+    }, [images])
 
     if(!images) return false
 
     function backClicked(){
-        if(pos > 0) setPos(pos - 1)
+        if(images.length > pos && pos > 0) setPos(pos - 1)
         else setPos(images.length - 1)
     }
 
     function nextClicked(){
-        if(pos < images.length - 1) setPos(pos + 1)
+        if(pos > 0 && pos < images.length - 1) setPos(pos + 1)
         else setPos(0)
     }
+
 
     return (
         <div className={styles.carousel}>
